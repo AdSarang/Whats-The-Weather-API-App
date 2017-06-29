793a74b702237e337fda1328eb218142fa45ec6e
Advait <advait@advaits-macbook-pro.local>
2017-06-29 19:43:49 +0530

    Initial Commit

---
+++ Whats The Weather Update 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats The Weather Update 2.0
+//
+//  Created by Advait on 29/06/17.
+//  Copyright © 2017 Advait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
