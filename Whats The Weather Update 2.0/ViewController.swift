12491493db0a2de3a7de3573cd3eb68677da9568
Advait <advait@advaits-macbook-pro.local>
2017-06-29 22:45:44 +0530

    Version 2.0

--- Whats The Weather Update 2.0/ViewController.swift
+++ Whats The Weather Update 2.0/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = " "
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q="+(myTextField.text?.replacingOccurrences(of: " ", with: "%20"))!+"&appid=c03a2c0aba62ffdc045b6fa1b0b2d252")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error as Any)
+            }
+            else{
+                if let userData = data {
+                    do{
+                        let jsonResult = try JSONSerialization.jsonObject(with: userData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(jsonResult)
+                        let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String
+                        
+                        DispatchQueue.main.sync(execute: {
+                            self.resultLabel.text = description
+
+                        })
+                        
+                    }
+                    catch{
+                        print("Failed to fetch json")
+                        self.resultLabel.text = "Something went wrong"
+                    }
+                }
+                else{
+                    self.resultLabel.text = "Something went wrong"
+                }
+            }
+            
+        }
+        task.resume()
+        
+
     }
 
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
